c7a167f7c50bd4eb3314a49b2ff6c07a4f4a5704
r0somak <mikolajakartel@gmail.com>
2019-11-19 21:24:39 +0100

    Dodanie działającego formularza rejestracji.

--- Assets/Scripts/Database/DbHelper.cs
+++ Assets/Scripts/Database/DbHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
@@ -16,71 +17,21 @@ public class DbHelper
         reference = FirebaseDatabase.DefaultInstance.RootReference;
     }
     
-    public void SaveNewUser(Models.User user)
+    public void SaveNewUser(FirebaseUser user)
     {
-        string json = JsonUtility.ToJson(user);
+        Models.User trueUser = new Models.User(user.UserId, user.DisplayName, user.Email);
+        string json = JsonUtility.ToJson(trueUser);
         Debug.Log("JSON: "+json);
-        reference.Child("users").Child(user.username).SetRawJsonValueAsync(json).ContinueWith(task =>
+        reference.Child("users").Child(user.UserId).SetRawJsonValueAsync(json).ContinueWith(task =>
         {
             if (task.IsFaulted)
             {
-                Debug.Log(user.userEmail+" Save to database not working");
+                Debug.Log(user.Email+" Save to database not working");
             }
-            else if(task.IsCompleted)
+            if(task.IsCompleted)
             {
-                Debug.Log(user.userEmail+" Saved to database");
+                Debug.Log(user.Email+" Saved to database");
             }
         });
-    }
-
-    public void CreateAuthUser(string email, string password)
-    {
-        var auth = FirebaseAuth.DefaultInstance;
-        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
-        {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-                if (CheckError(task.Exception, (int) Firebase.Auth.AuthError.EmailAlreadyInUse))
-                {
-                    // do whatever you want in this case
-                    Debug.LogError("Email already in use");
-                }
-                return;
-            }
-            // Firebase user has been created.
-            FirebaseUser newUser = task.Result;
-            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-                newUser.Email, newUser.UserId);
-        });
-    }
-    
-    static bool CheckError(AggregateException exception, int firebaseExceptionCode)
-    {
-        Firebase.FirebaseException fbEx = null;
-        foreach (Exception e in exception.Flatten().InnerExceptions)
-        {
-            fbEx = e as Firebase.FirebaseException;
-            if (fbEx != null)
-                break;
-        }
-
-        if (fbEx != null)
-        {
-            if (fbEx.ErrorCode == firebaseExceptionCode)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return false;
     }
 }
--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Proyecto26;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 using Firebase;
+using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 
 public class UiManager : MonoBehaviour
 {
+    private StringBuilder message = new StringBuilder("");
+    private List<string> messagesList = new List<string>
+    {
+        "Wrong email format\n",
+        "Password is too short, must be at least 6 characters\n",
+        "Passwords differ\n",
+        "Email is already in use\n"
+    };
+
     [Header("Main start panel")] 
     public Button loginButton;
     public Button registerButton;
@@ -37,6 +50,7 @@ public class UiManager : MonoBehaviour
     public InputField emailLoginInput;
     public InputField passwordLoginInput;
     public Button doLoginButton;
+
     
     // Start is called before the first frame update
     void Start()
@@ -45,14 +59,91 @@ public class UiManager : MonoBehaviour
         SetPanelVisibility(okPanel, false);
         SetPanelVisibility(badPanel, false);
         SetPanelVisibility(loginPanel, false);
-        
-        Button logBtn = loginButton.GetComponent<Button>();
+
+        Button registerBtn = registerButton.GetComponent<Button>();
+        registerBtn.onClick.AddListener(delegate { SetPanelVisibility(registerPanel, true); });
+
+        Button sendRegisterBtn = sendData.GetComponent<Button>();
+        sendRegisterBtn.onClick.AddListener(CheckRegisterCredentials);
+
+        Button tryAgain = badPanelButton.GetComponent<Button>();
+        tryAgain.onClick.AddListener(delegate { SetPanelVisibility(badPanel, false); });
+
+        Button okPanelBackButton = okPanelButton.GetComponent<Button>();
+        okPanelBackButton.onClick.AddListener(delegate
+        {
+            SetPanelVisibility(okPanel, false);
+            SetPanelVisibility(registerPanel, false);
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+//TODO: Dodanie paska postepu, AsyncOperation ?
+    private async void CheckRegisterCredentials()
+    {
+        string emailText = emailInputField.GetComponent<InputField>().text;
+        string passwordText = passwordInputField.GetComponent<InputField>().text;
+        string passwordConfirmText = passwordConfirmInputField.GetComponent<InputField>().text;
+        Text badMessage = panelBadMessage.GetComponent<Text>();
+        
+        Regex emailPattern = new Regex(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                                       + "@"
+                                       + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$");
+        Match emailMatch = emailPattern.Match(emailText);
+        
+        message.Clear();
+        
+        if (passwordText == passwordConfirmText && passwordText.Length >= 6 && emailMatch.Success)
+        {
+            DbHelper db = new DbHelper();
+            var auth = FirebaseAuth.DefaultInstance;
+            FirebaseUser newUsr;
+            try
+            {
+                newUsr = await auth.CreateUserWithEmailAndPasswordAsync(emailText, passwordConfirmText);
+            }
+            catch (Exception e)
+            {
+                if (e.InnerException != null && e.InnerException.ToString() == "Firebase.FirebaseException: The email address is already in use by another account.")
+                {
+                    Debug.Log("UWAGA UWAGA WYJATEK: "+e.InnerException);
+                    message.Append(messagesList[3]);
+                    badMessage.text = message.ToString();
+                    SetPanelVisibility(badPanel, true);
+                }
+                throw;
+            }
+            Debug.LogFormat("Firebase user created successfully: email: {0} userId: ({1}), displayName: {2}",
+                newUsr.Email, newUsr.UserId, newUsr.DisplayName);
+            db.SaveNewUser(newUsr);
+            SetPanelVisibility(okPanel, true);
+        }
+        else
+        {
+            Debug.Log("Credentials not ok");
+            if (passwordText != passwordConfirmText)
+            {
+                message.Append(messagesList[2]);
+                Debug.Log("Passwords are different");
+            }
+            if (passwordConfirmText.Length <= 5)
+            {
+                message.Append(messagesList[1]);
+                Debug.Log("Password is too short");
+            }
+            if (!emailMatch.Success)
+            {
+                message.Append(messagesList[0]);
+                Debug.Log("Wrong email format");
+            }
+            
+            badMessage.text = message.ToString();
+            SetPanelVisibility(badPanel, true);
+        }
     }
 
     private static void SetPanelVisibility(RectTransform panel, bool x)
