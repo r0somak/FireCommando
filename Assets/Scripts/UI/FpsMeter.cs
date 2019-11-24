a8cc464bb6eda7c853c01c0f17efc46eac29804b
r0somak <mikolajakartel@gmail.com>
2019-11-24 09:58:41 +0100

    Dodanie licznika FPS

---
+++ Assets/Scripts/UI/FpsMeter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class FpsMeter : MonoBehaviour
+    {
+        /* Public Variables */
+        public float frequency = 0.5f;
+ 
+        /* **********************************************************************
+     * PROPERTIES
+     * *********************************************************************/
+        public int FramesPerSec { get; protected set; }
+ 
+        /* **********************************************************************
+     * EVENT HANDLERS
+     * *********************************************************************/
+        /*
+     * EVENT: Start
+     */
+        private Text fps;
+        private void Start()
+        {
+            fps = gameObject.GetComponent<Text>();
+            StartCoroutine(Fps());
+        }
+ 
+        /*
+     * EVENT: FPS
+     */
+        private IEnumerator Fps() {
+            for(;;){
+                // Capture frame-per-second
+                int lastFrameCount = Time.frameCount;
+                float lastTime = Time.realtimeSinceStartup;
+                yield return new WaitForSeconds(frequency);
+                float timeSpan = Time.realtimeSinceStartup - lastTime;
+                int frameCount = Time.frameCount - lastFrameCount;
+ 
+                // Display it
+                FramesPerSec = Mathf.RoundToInt(frameCount / timeSpan);
+                fps.text = FramesPerSec.ToString() + " fps";
+            }
+        }
+    }
+}
